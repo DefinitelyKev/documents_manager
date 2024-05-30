ccc086cd84aa0366f6f71daf626445e03217561c
Kevin Nguyen <kevinnk259@gmail.com>
2024-05-30 20:19:30 +1000

    setting up the repo

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
 
-print("lol")
 
-print('Cool')
+try:
+    file = open("test_file.txt", 'r')
+    content = file.read()
+    print(content)
+except FileNotFoundError as e:
+    print(f"An error has occured: {e}")
